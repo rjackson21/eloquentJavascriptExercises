bf36c263673b919cc7689cf250da3c3cddb3a370
Regina Jackson <rjackson156@gmail.com>
2022-02-13 01:53:41 -0500

    FizzBizz Exercise

--- ex1.js
+++ ex1.js
@@ -1,6 +1,13 @@
+// Write a loop that makes seven calls to console.log to 
+// output the following triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
 for(let line = "#"; line.length <= 7; line += "#"){
     console.log(line)
 }
-
-
-
