2034c0972a8e414bcaa5d7ffa0c8b1e74060439a
Regina Jackson <rjackson156@gmail.com>
2022-02-15 21:43:52 -0500

    Chapter 3 Exercise

---
+++ min.js
@@ -0,0 +1,11 @@
+function min(a,b) {
+    if (a <= b) {
+        return a;
+    }
+    else ( a >= b)
+        return b;
+}
+
+console.log(min(12,15))
+console.log(min(15,15))
+console.log(min(32,1))
