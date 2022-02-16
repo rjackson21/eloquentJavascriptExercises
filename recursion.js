d09dd5f5445730fd6d8daa524defb64831e21056
Regina Jackson <rjackson156@gmail.com>
2022-02-15 21:59:49 -0500

    Chapter 3 Exercise

---
+++ recursion.js
@@ -0,0 +1,18 @@
+function isEven(n){
+    if(n === 0){
+        return true;
+    }
+    else if (n === 1){
+        return false;
+    }
+    else {
+        return isEven(n - 2);
+    }
+    
+}
+
+console.log(isEven(1))
+console.log(isEven(0))
+console.log(isEven(50))
+console.log(isEven(75))
+console.log(isEven(-1))
